ec638a85982ad2d8b5f3e207db3c5f696e76786b
App Academy Student <student@appacademy.io>
2017-08-04 11:26:34 -0700

    Implement PolyTreeNode class

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,5 +1,7 @@
 class PolyTreeNode
 
+  attr_writer :children
+
   def initialize(value = nil)
     @value = value
     @parent = nil
@@ -18,16 +20,36 @@ class PolyTreeNode
     @value
   end
 
-  # def parent=
-  #
-  # end
+  def add_child(child)
+    if child.is_a?(PolyTreeNode)
+       @children << child
+    end
+  end
+
+  def parent=(node)
+    if node.is_a?(PolyTreeNode)      
+      @parent = node #node.is_a?(PolyTreeNode) ? node : nil
+      node.add_child(self)
+    else
+      puts "Please use a node."
+    end
+  end
 
 end
 
 ## Test
 if __FILE__ == $PROGRAM_NAME
-  s = PolyTreeNode.new(1)
-  p s.value
-  s.value = 2
-  p s.value
+  s1 = PolyTreeNode.new(1)
+  # p s.value
+
+  ####
+  s2 = PolyTreeNode.new(5)
+  s.parent = s2
+
+  p s1
+  p s2
+
+  # s.parent = "dad" #should raise error
+  # p s.parent #should not happens
+
 end
