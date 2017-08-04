b8b10e0c8464bce70309817dcb40f0edaaca445c
App Academy Student <student@appacademy.io>
2017-08-04 10:59:52 -0700

    First commit

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,33 @@
 class PolyTreeNode
 
+  def initialize(value = nil)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent
+    @parent.dup
+  end
+
+  def children
+    @children.dup
+  end
+
+  def value
+    @value
+  end
+
+  # def parent=
+  #
+  # end
+
+end
+
+## Test
+if __FILE__ == $PROGRAM_NAME
+  s = PolyTreeNode.new(1)
+  p s.value
+  s.value = 2
+  p s.value
 end
